b4e07a6e02b88a990067dc0537c0a32020ed2f28
David Tauraso <david318@me.com>
2019-11-05 12:35:35 -0800

    Moved all the buttons to the Display component

--- src/components/DisplayComponents/Display.js
+++ src/components/DisplayComponents/Display.js
@@ -1,5 +1,24 @@
 import React from "react";
+import Numbers from "../ButtonComponents/NumberButtons/Numbers"
+import Operators from "../ButtonComponents/OperatorButtons/Operators"
+import Specials from "../ButtonComponents/SpecialButtons/Specials"
 
 const Display = () => {
-  return <div className="display">{/* Display any props data here */}</div>;
+  return <div >
+          <div className="display">{/* Display any props data here */}
+
+          </div>
+          <div>
+          <Specials />
+          <Numbers />
+
+          </div>
+          <div>
+          <Operators />
+
+          </div>
+
+  </div>;
 };
+
+export default Display;
