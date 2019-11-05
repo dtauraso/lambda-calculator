42b3f9b3e9074031fbdcef705356f568dad73bc7
David Tauraso <david318@me.com>
2019-11-05 12:16:23 -0800

    Operator buttons render

--- src/components/ButtonComponents/OperatorButtons/OperatorButton.js
+++ src/components/ButtonComponents/OperatorButtons/OperatorButton.js
@@ -1,9 +1,13 @@
 import React from "react";
 
-const OperatorButton = () => {
+const OperatorButton = (props) => {
+  // console.log(props)
   return (
-    <>
-      {/* Display a button element rendering the data being passed down from the parent container on props */}
-    </>
+    <button>
+      {/* Display a button element rendering the data being passed down from the parent container on props */
+      props.icon.value}
+    </button>
   );
 };
+
+export default OperatorButton;
--- src/components/ButtonComponents/OperatorButtons/Operators.js
+++ src/components/ButtonComponents/OperatorButtons/Operators.js
@@ -1,4 +1,6 @@
-import React from "react";
+import React, {useState} from "react";
+import OperatorButton from "./OperatorButton"
+import { operators } from '../../../data'
 
 //import any components needed
 
@@ -6,11 +8,19 @@ import React from "react";
 
 const Operators = () => {
   // STEP 2 - add the imported data to state
+
+  const [operatorIcons, setOperatorIcons] = useState(operators)
+  console.log(operatorIcons)
   return (
     <div>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
-       it any props needed by the child component*/}
+       it any props needed by the child component*/
+       operatorIcons.map((operatorIcon, index) => (
+         <OperatorButton key={index} icon={operatorIcon} />
+       ))}
     </div>
   );
 };
+
+export default Operators;
