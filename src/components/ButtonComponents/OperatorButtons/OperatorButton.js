b9207cce27d370bbe4ec9a7c397c4a4a71aae4d9
David Tauraso <david318@me.com>
2019-11-05 12:24:53 -0800

    Special buttons render

--- src/components/ButtonComponents/OperatorButtons/OperatorButton.js
+++ src/components/ButtonComponents/OperatorButtons/OperatorButton.js
@@ -5,7 +5,7 @@ const OperatorButton = (props) => {
   return (
     <button>
       {/* Display a button element rendering the data being passed down from the parent container on props */
-      props.icon.value}
+      props.icon.char}
     </button>
   );
 };
